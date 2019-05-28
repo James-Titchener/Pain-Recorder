605479d06edb6c39cc810d0635e98a62c2296744
James <jamestitchener@gmail.com>
2019-05-27 19:15:58 -0500

    Initial Commit

---
+++ Pain Recorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pain Recorder
+//
+//  Created by mac on 5/27/19.
+//  Copyright © 2019 James Titchener. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
